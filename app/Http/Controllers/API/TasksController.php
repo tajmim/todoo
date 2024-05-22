123f297fb70be6b7a9e59283af6f9970af86c29d
Shamima Tajmim <52608165+tajmim@users.noreply.github.com>
2024-05-22 14:45:25 +0600

    init

---
+++ app/Http/Controllers/API/TasksController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Models\Task;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class TasksController extends Controller
+{
+    public function index()
+    {
+        $todos = Task::all();
+        return response()->json($todos);
+    }
+
+    public function store(Request $request)
+    {
+
+        $request->validate([
+            'title' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'is_completed' => 'boolean'
+        ]);
+        // dd($request->all());
+
+        $todo = Task::create([
+            'title' => $request->title,
+            'description' => $request->description,
+            'is_completed' => $request->is_completed ?? false
+        ]);
+
+        return response()->json($todo,201);
+    }
+    public function show($id)
+    {
+        $task = Task::findOrFail($id);
+        return response()->json($task, 200);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $task = Task::findOrFail($id);
+        $request->validate([
+            'title' => 'string|max:255',
+            'description' => 'nullable|string',
+            'is_completed' => 'boolean'
+        ]);
+
+        $task->update($request->all());
+
+        return response()->json($task);
+    }
+    public function destroy($id)
+    {
+        Task::findOrFail($id)->delete();
+        return response()->json(null, 204);
+    }
+}
